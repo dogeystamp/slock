9c4ab9e67b7b684bec2bf511ae6e8eb1719a063a
DogeyStamp <dogeystamp@disroot.org>
2021-10-09 15:34:25 -0400

    Remove config.h

--- config.h
+++ config.h
@@ -11,6 +11,13 @@ static const char *colorname[NUMCOLS] = {
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
 
+/* number of failed password attempts until failcommand is executed.
+   Set to 0 to disable */
+static const int failcount = 5;
+
+/* command to be executed after [failcount] failed password attempts */
+static const char *failcommand = "loginctl poweroff";
+
 /* time in seconds to cancel lock with mouse movement */
 static const int timetocancel = 4;
 /* allow control key to trigger fail on clear */
